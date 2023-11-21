6eb0579b7452ae5b22de64b3e6441167685d62e7
Ileis <gabrielileis07@gmail.com>
2023-11-21 11:52:56 -0300

    Correção aStar

--- src/Algorithms/aStar.java
+++ src/Algorithms/aStar.java
@@ -1,7 +1,6 @@
 package Algorithms;
 
 import java.util.Comparator;
-
 import Structures.City.City;
 import Structures.Graph.Graph;
 import Structures.Graph.Edge;
@@ -10,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class aStar {
+
     private static class Node <T>{
         public T key;
         public Node<T> parent;
@@ -25,6 +25,11 @@ public class aStar {
 
         public float getFn(){
             return this.gn + this.hn;
+        }
+
+        @Override
+        public String toString() {
+            return key.toString() + "-> f(n):" + this.getFn();
         }
     }
 
@@ -45,21 +50,19 @@ public class aStar {
 
             if(border.isEmpty()) break;
 
-            node = border.poll();
-
+            node = border.poll(); 
             // System.out.println(node.key + ", fn:" + node.getFn() + ", gn:" + node.gn + ", hn:" + node.hn);
 
-            ArrayList<City> path = getPath(node);
+            ArrayList<City> path = getPath(node); // O(n)
 
-            if(path.size() == (vertices.size() + 1) && node.key.equals(initialCity)) break;
+            if(path.size() == (vertices.size() + 1) && node.key.equals(initialCity)) break; // O(1)
 
-            ArrayList<Edge<City>> neighbors = map.getAllNeighborEdges(node.key);
-            ArrayList<City> verticesInducedSubgraph = new ArrayList<City>(vertices);
-            verticesInducedSubgraph.removeAll(path);
-            verticesInducedSubgraph.add(initialCity);
+            ArrayList<Edge<City>> neighbors = map.getAllNeighborEdges(node.key); // O(n)
+            ArrayList<City> verticesInducedSubgraph = new ArrayList<City>(vertices); // O(n)
+            verticesInducedSubgraph.removeAll(path); // O(n * m)
+            verticesInducedSubgraph.add(initialCity); // O(1)
 
-            float hnNode = AGM.costKruskal(Graph.inducedSubgraphByVertices(edges, verticesInducedSubgraph));
-            // System.out.println(edges);
+            float hnNode = AGM.costKruskal(Graph.inducedSubgraphByVertices(edges, verticesInducedSubgraph)); // O(n^2) + O(n log n)
 
             // se edge.getSecondVertex está na lista mas apresenta um peso maior, substitua
             for(Edge<City> edge : neighbors){
@@ -69,6 +72,8 @@ public class aStar {
                     float gnNode = map.getCost(node.key, edge.getSecondVertex()) + node.gn;
 
                     border.add(new Node<City>(edge.getSecondVertex(), node, gnNode, hnNode));
+
+                    // addAndRemoveAllBigger(border, new Node<City>(edge.getSecondVertex(), node, gnNode, hnNode));
                 }
             }
         }
@@ -78,6 +83,27 @@ public class aStar {
 
         System.out.print(path);
         System.out.println(" " + String.format("%f", pathCost));
+    }
+
+    public static boolean cityIsInBorder(PriorityQueue<Node<City>> priorityQueue, City c){
+        for(Node<City> node : priorityQueue){
+            if(node.key.equals(c)) return true;
+        }
+        return false;
+    }
+
+    public static void addAndRemoveAllBigger(PriorityQueue<Node<City>> priorityQueue, Node<City> newNode){
+
+        ArrayList<Node<City>> removedNodes = new ArrayList<Node<City>>();
+
+        for(Node<City> node : priorityQueue){
+            if(node.key.equals(newNode.key) && newNode.getFn() < node.getFn()) removedNodes.add(node);
+        }
+
+        // System.out.println("NewNode:" + newNode + removedNodes);
+
+        priorityQueue.removeAll(removedNodes);
+        priorityQueue.add(newNode);
     }
 
     public static <T> ArrayList<T> getPath(Node<T> node){
